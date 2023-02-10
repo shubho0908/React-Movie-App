d026015d32248822e226ee0571d0dcdf19250cf2
Shubhojeet Bera <shubhobera98@gmail.com>
2023-02-10 19:22:22 +0530

    router setup

--- src/App.js
+++ src/App.js
@@ -1,9 +1,23 @@
-import React from 'react'
+import React from "react";
+import Home from "./Home";
+import DetailPage from './DetailPage'
+import Error from "./Error";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 const App = () => {
   return (
-    <div>App</div>
-  )
-}
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route exact path="/" element={<Home/>}/>
+          
+          {/* The "id" in the path can be anything */}
+          <Route exact path="movie/:id" element={<DetailPage/>}/>
+          <Route exact path="*" element={<Error/>}/>
+        </Routes>
+      </BrowserRouter>
+    </>
+  );
+};
 
-export default App
+export default App;
--- src/DetailPage.js
+++ src/DetailPage.js
@@ -1,8 +1,10 @@
 import React from 'react'
+import { useParams } from 'react-router-dom'
 
 const DetailPage = () => {
+  const {id} = useParams()
   return (
-    <div>DetailPage</div>
+    <div>You searched for <b>{id}</b> movie.</div>
   )
 }
 
