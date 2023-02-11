d2447e28b2d5dff4e59ae93ef349c1f8eb3cfa1f
Shubhojeet Bera <shubhobera98@gmail.com>
2023-02-11 12:08:59 +0530

    OMDB API integrated

--- src/App.js
+++ src/App.js
@@ -1,17 +1,19 @@
 import React from "react";
 import Home from "./Home";
-import DetailPage from './DetailPage'
+import DetailPage from "./DetailPage";
 import Error from "./Error";
-import { Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 const App = () => {
   return (
     <>
+      <BrowserRouter>
         <Routes>
-          <Route exact path="/" element={<Home/>}/>
-          <Route exact path="movie/:id" element={<DetailPage/>}/>
-          <Route exact path="*" element={<Error/>}/>
+          <Route exact path="/" element={<Home />} />
+          <Route exact path="movie/:id" element={<DetailPage />} />
+          <Route exact path="*" element={<Error />} />
         </Routes>
+      </BrowserRouter>
     </>
   );
 };
--- src/context.js
+++ src/context.js
@@ -1,7 +1,23 @@
-import React, { useContext } from "react";
+import React, { useContext, useEffect } from "react";
+const API_URL = `http://www.omdbapi.com/?apikey=20d09b0a&s=pathaan`;
+
 const AppContext = React.createContext();
 
 const AppProvider = ({ children }) => {
+  const getMovies = async (url) => {
+    try {
+      const res = await fetch(url);
+      const data = await res.json();
+      console.log(data);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    getMovies(API_URL);
+  }, []);
+
   return (
     <AppContext.Provider value="Shubho">
       {children}
@@ -12,8 +28,8 @@ const AppProvider = ({ children }) => {
 
 //Global Context-
 //Just to simplify the code
-const GlobalContext = ()=>{
-    return useContext(AppContext)
-}
+const GlobalContext = () => {
+  return useContext(AppContext);
+};
 
-export { AppContext, AppProvider,GlobalContext };
+export { AppContext, AppProvider, GlobalContext };
