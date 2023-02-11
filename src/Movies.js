b7ef29a0f93010e33024ad24013a093c0e342301
Shubhojeet Bera <shubhobera98@gmail.com>
2023-02-11 16:08:05 +0530

    major responsiveness fixed

--- src/Movies.js
+++ src/Movies.js
@@ -8,18 +8,30 @@ const Movies = () => {
     <>
       <div className="movie-page">
         <div className="container grid grid-4-col">
-        {Movies.map((element) => {
-        return (
-          <NavLink to={`movie/${element.imdbID}`} key={element.imdbID}>
-          <div className="card" >
-            <div className="card-info">
-              <img className="poster" src={element.Poster} alt={element.Title + "'s poster"} />
-              <h2>{(element.Title).length <= 30 ? (element.Title) : (element.Title.slice(0, 30) + "..")}</h2>
-            </div>
-          </div>
-          </NavLink>
-        );
-      })}
+          {Movies.map((element) => {
+            return (
+              <NavLink to={`movie/${element.imdbID}`} key={element.imdbID}>
+                <div className="card">
+                  <div className="card-info">
+                    <p className="hd-tag">HD</p>
+                    <img
+                      className="poster"
+                      src={element.Poster}
+                      alt={element.Title + "'s poster"}
+                    />
+                    <div className="about-movie">
+                      <h2>
+                        {element.Title.length <= 20
+                          ? element.Title
+                          : element.Title.slice(0, 20) + ".."}
+                      </h2>
+                      <h2 className="type">{element.Type}</h2>
+                    </div>
+                  </div>
+                </div>
+              </NavLink>
+            );
+          })}
         </div>
       </div>
     </>
