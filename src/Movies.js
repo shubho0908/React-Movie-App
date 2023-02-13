dd3e724664233a74607c413554179fbd16c545a1
Shubhojeet Bera <shubhobera98@gmail.com>
2023-02-13 18:05:33 +0530

    updated

--- src/Movies.js
+++ src/Movies.js
@@ -2,26 +2,35 @@ import React from "react";
 import { NavLink } from "react-router-dom";
 import { GlobalContext } from "./context";
 import Footer from "./Footer";
-import errorimg from './error-img.jpg'
+import errorimg from "./error-img.jpg";
 
 const Movies = () => {
-  const { Movies, search,IsLoading, IsError } = GlobalContext(); //The variables name should be as same the Hook, e.g. "Movies"
+  const { Movies, search, IsLoading, IsError } = GlobalContext(); //The variables name should be as same the Hook, e.g. "Movies"
   if (IsLoading) {
     return (
       <div>
-        <div className="loading">{IsError.msg === "Movie not found!" || IsError.msg === "Incorrect IMDb ID." ? "" : "Loading..."}</div>
+        <div className="loading">
+          {IsError.msg === "Movie not found!" ||
+          IsError.msg === "Incorrect IMDb ID."
+            ? ""
+            : "Loading..."}
+        </div>
       </div>
     );
   }
 
-  document.title = `Movie HD`
-
+  document.title = `Movie HD`;
 
   return (
     <>
       <div className="movie-page">
         <h3 className="results">
-          Results for <b><u><em>{search}</em></u></b>
+          Results for{" "}
+          <b>
+            <u>
+              <em>{search}</em>
+            </u>
+          </b>
         </h3>
         <div className="container grid grid-4-col">
           {Movies.map((element) => {
@@ -30,18 +39,23 @@ const Movies = () => {
               <NavLink to={`movie/${element.imdbID}`} key={element.imdbID}>
                 <div className="card">
                   <div className="card-info">
-                    <p className="hd-tag">{element.Poster === "N/A"? "" : "HD"}</p>
+                    <p className="hd-tag">
+                      {element.Poster === "N/A" ? "" : "HD"}
+                    </p>
                     <img
                       className="poster"
-                      src={element.Poster === "N/A"? errorimg : element.Poster}
+                      src={element.Poster === "N/A" ? errorimg : element.Poster}
                       alt={MovieTitle + "'s poster"}
                     />
                     <div className="about-movie">
-                      <h2>
-                        {MovieTitle.length >= 15
-                          ? `${MovieTitle}...`
-                          : MovieTitle}
-                      </h2>
+                      <div className="mv-data">
+                        <h2>
+                          {MovieTitle.length >= 15
+                            ? `${MovieTitle}...`
+                            : MovieTitle}
+                        </h2>
+                        <p className="mv-year">{element.Year}</p>
+                      </div>
                       <h2 className="type">{element.Type}</h2>
                     </div>
                   </div>
@@ -51,7 +65,7 @@ const Movies = () => {
           })}
         </div>
       </div>
-      <Footer/>
+      <Footer />
     </>
   );
 };
